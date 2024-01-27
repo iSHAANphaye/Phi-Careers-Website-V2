105272cebfae54d4ece59003aa7434ba4245b9cc
Ishaan Phaye <ishaanphaye@gmail.com>
2024-01-27 17:31:44 +0530

    Added dynamic pages for each job

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # To run server, use command: flask --app app run --debug
 
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 
 app = Flask(__name__)
 
@@ -17,3 +17,13 @@ def hello_html():
 def list_jobs():
     jobs=load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job=load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html',job=job)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,23 +1,29 @@
 import pymysql
 
-def load_jobs_from_db():
-    timeout = 10
-    connection = pymysql.connect(
-        charset="utf8mb4",
-        connect_timeout=timeout,
-        cursorclass=pymysql.cursors.DictCursor,
-        db="phicareers",
-        host="mysql-phicareers-phicareers.a.aivencloud.com",
-        password="AVNS__MjBOcYMMK0yhk0J52T",
-        read_timeout=timeout,
-        port=27509,
-        user="avnadmin",
-        write_timeout=timeout,
-    )
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="phicareers",
+    host="mysql-phicareers-phicareers.a.aivencloud.com",
+    password="AVNS__MjBOcYMMK0yhk0J52T",
+    read_timeout=timeout,
+    port=27509,
+    user="avnadmin",
+    write_timeout=timeout,
+)
 
+def load_jobs_from_db():
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM jobs")  # Imports the jobs table
     jobs=[]
     for row in cursor.fetchall():
         jobs.append(row)
     return jobs
+
+def load_job_from_db(uid):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM jobs WHERE id = %s", (uid,))
+    info= cursor.fetchone()
+    return info
